2593ae505f48d5ba7b052ab06080c593d1c255ea
Gaël La Rochelle <gael_coupe17@outlook.fr>
2023-12-04 21:44:59 +0100

    Modif package.json

--- src/main.js
+++ src/main.js
@@ -4,18 +4,7 @@ import router from './router'
 import store from './store'
 import './assets/tailwind.css'
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
-library.add(faUserSecret)
-
-createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+createApp(App)
 .use(store).use(router).mount('#app')
  
